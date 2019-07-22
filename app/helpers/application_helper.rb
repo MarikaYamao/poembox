bf97f463518feaca3d958701867c4bc84f60ac13
EC2 Default User <ec2-user@ip-172-31-43-39.ap-northeast-1.compute.internal>
2019-07-22 08:55:16 +0000

    twitter

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,29 @@
 module ApplicationHelper
-
+  def default_meta_tags
+    {
+      site: 'Poembox',
+      title: 'Poembox',
+      reverse: true,
+      charset: 'utf-8',
+      description: 'Post Poem With Emotional Image',
+      keywords: 'poem,photo,image',
+      canonical: request.original_url,
+      separator: '|',
+      viewport: 'width=device-width, initial-scale=1',
+      icon: { href: image_url('/favicon.ico') },
+      og: {
+        site_name: :site,
+        title: :title,
+        description: :description,
+        type: 'website',
+        url: request.original_url,
+        image: { href: image_url('/cover-bg.jpg') },
+        locale: 'ja_JP',
+      },
+      twitter: {
+        card: 'summary_large_image',
+        site: '@poembox',
+      }
+    }
+  end
 end
